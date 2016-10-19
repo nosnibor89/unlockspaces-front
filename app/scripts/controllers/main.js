aaf71f885bd8cc4606673973838fb3b5a136ef81
Robinson <nosnibor1989@gmail.com>
2016-10-19 18:29:31 -0400

    Spaces Pagination
    AngularUI pagnation directive integration.
    Customizing AngularUI pagnation directive.
    Pagination integration in MainController.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,26 +10,46 @@
    * control behavior of the main page, like pagination, 
    * databinding, etc.
    */
-  function MainCtrl($scope, usData) {
+  function MainCtrl($scope, $uibModal, usData, usSpacePager) {
+
+    var main = this;
+    main.perPage = 4;
+    main.currentPage = 1;
 
     /**
-     * Place spaces in scope once the object are succesfully returned
+     * Place spaces in scope once the object are succesfully returned.
+     * Puts in scope, the space for the first page.
      * 
      * @param {any} data
      */
     function onSpacesSuccess(data) {
-      $scope.spaces = data;
-    }
+      main.spaces = data;
+      main.totalItems = main.spaces.length;
+      main.viewSpaces = usSpacePager.getPageItems(main.currentPage, main.perPage, main.spaces);
+    };
 
-    function onSpacesError(data){
+    /**
+     * Show error to the user
+     * 
+     * @param {any} data
+     */
+    function onSpacesError(data) {
 
-    }
+    };
+
+    /**
+     * Handler function that handles when page is change.
+     */
+    $scope.pageChanged = function () {
+      main.viewSpaces = usSpacePager.getPageItems(main.currentPage, main.perPage, main.spaces);
+    };
 
     //Get spaces from the API
     usData.space.query(null, onSpacesSuccess, onSpacesError);
+
   };
 
   angular.module('unlockspacesFrontApp')
-    .controller("MainCtrl", ["$scope", "usData", MainCtrl]);
+    .controller("MainCtrl", ["$scope", "$uibModal", "usData", "usSpacePager", MainCtrl]);
 
 })();
--- app/scripts/services/usdata.js
+++ app/scripts/services/usdata.js
@@ -1,26 +1,25 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc service
- * @name unlockspacesFrontApp.usData
- * @description
- * # usData
- * Factory in the unlockspacesFrontApp. Helps to get data from the api
- */
-function usData($resource, CONFIG) {
+  /**
+   * @ngdoc service
+   * @name unlockspacesFrontApp.usData
+   * @description
+   * # usData
+   * Factory in the unlockspacesFrontApp. Helps to get data from the api
+   */
+  function usData($resource, CONFIG) {
+    // Service logic
+    var space = $resource(CONFIG.WEBAPI.BASEURL);
 
-  console.log(CONFIG.WEBAPI.BASEURL);
-  // Service logic
-  var space = $resource(CONFIG.WEBAPI.BASEURL);
+    // Public API here
+    var service = {
+      space: space
+    };
+    return service;
+  }
 
+  angular.module('unlockspacesFrontApp')
+    .factory('usData', ["$resource", "CONFIG", usData]);
 
-
-  // Public API here
-  var service = {
-    space: space
-  };
-  return service;
-}
-
-angular.module('unlockspacesFrontApp')
-  .factory('usData', ["$resource", "CONFIG", usData]);
+})();
